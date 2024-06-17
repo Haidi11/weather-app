e136b3cbbc2133de41b44219dfc88372481f5fa9
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2024-06-17 12:02:20 +0200

    Updated weather service and model.

--- src/app/shared/models/weather.model.ts
+++ src/app/shared/models/weather.model.ts
@@ -1,16 +1,18 @@
 export interface Weather {
-    list: Array<{
-      dt: number;
-      main: {
-        temp: number;
-        temp_min: number;
-        temp_max: number;
-      };
-      weather: Array<{
-        description: string;
-        icon: string;
-      }>;
-      dt_txt: string;
+  city: string;
+  temperature: number;
+  list: Array<{
+    dt: number;
+    main: {
+      temp: number;
+      temp_min: number;
+      temp_max: number;
+    };
+    weather: Array<{
+      description: string;
+      icon: string;
     }>;
-  }
+    dt_txt: string;
+  }>;
+}
   
