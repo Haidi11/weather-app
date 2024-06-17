f6ca9e4aa84895f9e812486ade29e646374f0089
Haidi11 <39265042+Haidi11@users.noreply.github.com>
2024-06-17 12:02:05 +0200

    Updated app-component and added loader.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { WeatherService } from './shared/service/weather.service';
 import { ForecastComponent } from './shared/components/forecast/forecast.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ForecastComponent],
+  imports: [CommonModule, ForecastComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = "test";
   isLoading: boolean = true;
-  weatherData: any;
 
-  constructor(private weatherService: WeatherService) { }
-
-  ngOnInit(): void {
-    this.getWeather('Maribor');
+  constructor() {
   }
 
-  getWeather(location: string): void {
-    this.weatherService.getWeather(location).subscribe(
-      (data: any) => {
-        this.weatherData = data;
-        this.isLoading = false;
-      },
-      (error: any) => {
-        console.error('Error fetching weather data', error);
-        this.isLoading = false;
-      }
-    );
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 }
